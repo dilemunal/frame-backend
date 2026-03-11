40cda6ee1f84901b4c3a342eb60e4cdc4b3f3dab
Dilem <diilemunal@gmail.com>
2026-03-11 13:41:30 +0300

    inital

---
+++ src/main/java/com/dilem/framebackend/FrameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dilem.framebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dilem/framebackend/FrameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilem.framebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
